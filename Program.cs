4230f7e2c089f25d2f1fc47586918989721f6b00
pranaliandre <pranaliandre06@gmail.com>
2020-03-31 13:06:24 -0500

    Added: Swtich cse to decide part or full time

--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@ namespace empWageComputationProgram
         private static int EMP_PART_TIME = 1;
         //variable
         private static int wagePerDay = 0;
+        private static int workHours=0;
         static void Main(string[] args)
         {
             Program emp =new Program();
@@ -20,22 +21,36 @@ namespace empWageComputationProgram
             Random random = new Random();
             int isEmpRandom = random.Next(0, 3);
             int empType = (isEmpRandom % 3);
-            int wage=emp.employeeType(empType);
+            //calculating full-time,part-time using case statement 
+            switch (empType)
+            {
+                case 1:
+                    workHours = EMP_PART_TIME_HOUR;
+                    break;
+                case 2:
+                    workHours = EMP_FULL_DAY_HOUR;
+                    break;
+                default:
+                    break;
+            }
+
+            int wage=emp.employeeType(empType,EMP_WAGE_PER_HOUR,workHours);
             Console.WriteLine("wage per day : " + wage);
         }
         //Method to check employee present full time or part time then calulate the wagePerDay
-        public int employeeType(int empType)
+        public int employeeType(int empType,int EMP_WAGE_PER_HOUR, int workHours)
         {
+            
             if ( empType == EMP_FULL_TIME)
             {
                 Console.WriteLine("Employee is present for full time");
-                wagePerDay = EMP_WAGE_PER_HOUR * EMP_FULL_DAY_HOUR;
+                wagePerDay = EMP_WAGE_PER_HOUR * workHours;
                 return wagePerDay;
             }
             else if( empType == EMP_PART_TIME )
             {
                 Console.WriteLine("Employee is present for part time");
-                wagePerDay = EMP_WAGE_PER_HOUR * EMP_PART_TIME_HOUR;
+                wagePerDay = EMP_WAGE_PER_HOUR * workHours;
                 return wagePerDay;
             }
             else
