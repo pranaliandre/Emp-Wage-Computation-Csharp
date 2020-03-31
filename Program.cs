3f49d8b13e3e47707833302b7102ffab1b9cc73a
pranaliandre <pranaliandre06@gmail.com>
2020-03-31 04:13:56 -0500

    Added: welcome

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace empWageComputationProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee wage computation");
+        }
+    }
+}
