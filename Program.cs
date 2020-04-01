7a898537435b67ae86811e9eecb153ad9780a1c9
pranaliandre <pranaliandre06@gmail.com>
2020-04-01 05:04:20 -0500

    Added: Calculate wage till a condition of total working hours or day is reached for a month

--- Program.cs
+++ Program.cs
@@ -12,28 +12,32 @@ namespace empWageComputationProgram
         private static int EMP_FULL_TIME = 2;
         private static int EMP_PART_TIME = 1;
         private static int NUM_OF_WORKING = 20;
+        private static int MONTHLY_MAX_WORKING_HOURS = 100;
         //variable
+        private static int totalEmpHrs = 0;
+        private static int totalWorkingDay = 0;
         private static int wagePerDay = 0;
         private static int empType;
         private static int isEmpRandom;
         private static int workHours=0;
-        private static int totalWage = 0;
+        private static int totalWage;
         Random random = new Random();
        
         static void Main(string[] args)
         {
             Program emp =new Program();
             Console.WriteLine("Welcome to Employee Wage Computation");
-            int empMonthlyWage=emp.employeeType();
+            int empMonthlyWage=emp.empTotalWorkHoursDay();
             Console.WriteLine("wage per day : " + empMonthlyWage);
         }
-        //Method to check employee present full time or part time then calulate the employee monthly wage
-        public int employeeType()
+        //Method to Calculate Wages till a condition of total working hours or days is reached for a month 
+        public int empTotalWorkHoursDay()
         {
-            for (int dayCount = 1; dayCount <= NUM_OF_WORKING; dayCount++)
+            while (totalEmpHrs < MONTHLY_MAX_WORKING_HOURS && totalWorkingDay < NUM_OF_WORKING)
             {
+                totalWorkingDay++;
                 isEmpRandom = random.Next(0, 3);
-                int empType = (isEmpRandom % 3);
+                empType = (isEmpRandom % 3);
                 switch (empType)
                 {
                     case 1:
@@ -60,11 +64,14 @@ namespace empWageComputationProgram
                 {
                     Console.WriteLine("Emloyee is absent");
                     wagePerDay = NO_WAGE;
-     
+
                 }
                 totalWage = totalWage + wagePerDay;//calculate total wage 
+
             }
+            ////return employee wage for month 
             return totalWage;
+           
         }
     }
 }
